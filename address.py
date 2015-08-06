dd5146ef24a1fce2076879ab013a22c90679d1d4
Tarun Bhardwaj <mailme@tarunbhardwaj.com>
2015-08-06 14:28:15 +0530

    Code will be maintained by Fulfil.IO Inc

--- address.py
+++ address.py
@@ -2,8 +2,6 @@
 """
     address.py
 
-    :copyright: (c) 2014 by Openlabs Technologies & Consulting (P) Limited
-    :license: BSD, see LICENSE for more details.
 """
 from trytond.pool import PoolMeta
 
